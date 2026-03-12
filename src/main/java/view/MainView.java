1fda8db3e641238e80fbb1242a364d909343e39f
Julien BONNAFE <j.bonnafe@lyceeclaudenougaro82300.onmicrosoft.com>
2026-03-12 12:24:07 +0100

    init début de projet ajout card Layout

--- src/main/java/view/MainView.java
+++ src/main/java/view/MainView.java
@@ -4,6 +4,8 @@
  */
 package view;
 
+import java.awt.CardLayout;
+
 /**
  *
  * @author m.arakelian
@@ -26,20 +28,76 @@ public class MainView extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel1 = new javax.swing.JPanel();
+        MainPanel = new javax.swing.JPanel();
+        JPanelCommune = new javax.swing.JPanel();
+        JPanelUtilisateur = new javax.swing.JPanel();
+        jMenuBar1 = new javax.swing.JMenuBar();
+        jMenu1 = new javax.swing.JMenu();
+        jMenu2 = new javax.swing.JMenu();
+        CheckBoxUtilisateur = new javax.swing.JCheckBoxMenuItem();
+        CheckBoxCommune = new javax.swing.JCheckBoxMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        MainPanel.setLayout(new java.awt.CardLayout());
+
+        JPanelCommune.setBackground(new java.awt.Color(242, 242, 42));
+
+        javax.swing.GroupLayout JPanelCommuneLayout = new javax.swing.GroupLayout(JPanelCommune);
+        JPanelCommune.setLayout(JPanelCommuneLayout);
+        JPanelCommuneLayout.setHorizontalGroup(
+            JPanelCommuneLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 562, Short.MAX_VALUE)
         );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+        JPanelCommuneLayout.setVerticalGroup(
+            JPanelCommuneLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 413, Short.MAX_VALUE)
         );
+
+        MainPanel.add(JPanelCommune, "CardNameCommune");
+
+        JPanelUtilisateur.setBackground(new java.awt.Color(0, 0, 0));
+
+        javax.swing.GroupLayout JPanelUtilisateurLayout = new javax.swing.GroupLayout(JPanelUtilisateur);
+        JPanelUtilisateur.setLayout(JPanelUtilisateurLayout);
+        JPanelUtilisateurLayout.setHorizontalGroup(
+            JPanelUtilisateurLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 562, Short.MAX_VALUE)
+        );
+        JPanelUtilisateurLayout.setVerticalGroup(
+            JPanelUtilisateurLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 413, Short.MAX_VALUE)
+        );
+
+        MainPanel.add(JPanelUtilisateur, "CardNameUtilisateur");
+
+        jMenu1.setText("File");
+        jMenuBar1.add(jMenu1);
+
+        jMenu2.setText("Edit");
+
+        CheckBoxUtilisateur.setSelected(true);
+        CheckBoxUtilisateur.setText("CheckBoxUtilisateur");
+        CheckBoxUtilisateur.setActionCommand("");
+        CheckBoxUtilisateur.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                CheckBoxUtilisateurActionPerformed(evt);
+            }
+        });
+        jMenu2.add(CheckBoxUtilisateur);
+
+        CheckBoxCommune.setSelected(true);
+        CheckBoxCommune.setText("jCheckBoxMenuItem2");
+        CheckBoxCommune.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                CheckBoxCommuneActionPerformed(evt);
+            }
+        });
+        jMenu2.add(CheckBoxCommune);
+
+        jMenuBar1.add(jMenu2);
+
+        setJMenuBar(jMenuBar1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -47,53 +105,44 @@ public class MainView extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(MainPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(MainPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void CheckBoxCommuneActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_CheckBoxCommuneActionPerformed
+        // TODO add your handling code here:
+        CardLayout card = (CardLayout)MainPanel.getLayout();
+        card.show(MainPanel, "CardNameUtilisateur");
+        
+    }//GEN-LAST:event_CheckBoxCommuneActionPerformed
+
+    private void CheckBoxUtilisateurActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_CheckBoxUtilisateurActionPerformed
+        // TODO add your handling code here:
+        CardLayout card = (CardLayout)MainPanel.getLayout();
+        card.show(MainPanel, "CardNameCommune");
+        
+    }//GEN-LAST:event_CheckBoxUtilisateurActionPerformed
+
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(MainView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(MainView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(MainView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(MainView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
 
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new MainView().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JPanel jPanel1;
+    private javax.swing.JCheckBoxMenuItem CheckBoxCommune;
+    private javax.swing.JCheckBoxMenuItem CheckBoxUtilisateur;
+    private javax.swing.JPanel JPanelCommune;
+    private javax.swing.JPanel JPanelUtilisateur;
+    private javax.swing.JPanel MainPanel;
+    private javax.swing.JMenu jMenu1;
+    private javax.swing.JMenu jMenu2;
+    private javax.swing.JMenuBar jMenuBar1;
     // End of variables declaration//GEN-END:variables
 }
