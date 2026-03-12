446879b4eebc716a0aa87061a2a8fef26ac7784a
Julien BONNAFE <j.bonnafe@lyceeclaudenougaro82300.onmicrosoft.com>
2026-03-12 12:31:02 +0100

    ajout view utilisateur et Commune

--- src/main/java/view/CommuneView.java
+++ src/main/java/view/CommuneView.java
@@ -8,12 +8,12 @@ package view;
  *
  * @author j.bonnafe
  */
-public class UtilisateurView extends javax.swing.JFrame {
+public class CommuneView extends javax.swing.JFrame {
 
     /**
-     * Creates new form UtilisateurView
+     * Creates new form CommuneView
      */
-    public UtilisateurView() {
+    public CommuneView() {
         initComponents();
     }
 
@@ -59,20 +59,20 @@ public class UtilisateurView extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(UtilisateurView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CommuneView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(UtilisateurView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CommuneView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(UtilisateurView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CommuneView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(UtilisateurView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(CommuneView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new UtilisateurView().setVisible(true);
+                new CommuneView().setVisible(true);
             }
         });
     }
