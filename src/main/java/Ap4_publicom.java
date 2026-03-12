1c690ce8131de0e82725730ac0ea794b358eb5d1
Mathieuarak <mathieuarakelian193@gmail.com>
2026-03-12 10:31:27 +0100

    Add files via upload

---
+++ src/main/java/Ap4_publicom.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author m.arakelian
+ */
+public class Ap4_publicom {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
